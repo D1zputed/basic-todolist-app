78af33ebdac5ad006438f2b1a4ae2d69caea9aba
D1zputed <asaphdelcoro987@gmail.com>
2026-07-23 16:19:59 +0800

    added constructor and methods

--- BasicTodoList/ViewModels/ToDoItemViewModel.cs
+++ BasicTodoList/ViewModels/ToDoItemViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BasicTodoList.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,11 @@ namespace BasicTodoList.ViewModels
 {
     public partial class ToDoItemViewModel : ViewModelBase
     {
+        /// <summary>
+        /// Gets or sets the checked status of each item
+        /// </summary>
+        // NOTE: This property is made without source generator. Uncomment the line below to use the source generator
+        // [ObservableProperty]
         private bool _isChecked;
 
         public bool IsChecked
@@ -15,7 +21,42 @@ namespace BasicTodoList.ViewModels
             set { SetProperty(ref _isChecked, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the content of the to-do item
+        /// </summary>
         [ObservableProperty]
         public partial string? Content { get; set; }
+
+        /// <summary>
+        /// Creates a new blank ToDoItemViewModel
+        /// </summary>
+        public ToDoItemViewModel()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new ToDoItemViewModel for the given <see cref="Models.ToDoItem"/>
+        /// </summary>
+        /// <param name="item">The item to load</param>
+        public ToDoItemViewModel(ToDoItem item)
+        {
+            // Init the properties with the given values
+            IsChecked = item.IsChecked;
+            Content = item.Content;
+        }
+
+        /// <summary>
+        /// Gets a ToDoItem of this ViewModel
+        /// </summary>
+        /// <returns>The ToDoItem</returns>
+        public ToDoItem GetTodoItem()
+        {
+            return new ToDoItem()
+            {
+                IsChecked = this.IsChecked,
+                Content = this.Content
+            };
+        }
     }
 }
