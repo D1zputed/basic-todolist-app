45d55b506398a39dbe8dd69fe854291bfde0f782
D1zputed <asaphdelcoro987@gmail.com>
2026-07-23 10:56:58 +0800

    added ToDoItem model and ToDoItemViewModel

---
+++ BasicTodoList/Models/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicTodoList.Models
+{
+    public class ToDoItem
+    {
+        /// <summary>
+        /// Gets or sets the checked status of each item
+        /// </summary>
+        public bool IsChecked { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the content of each item
+        /// </summary>
+        public string? Content { get; set; }
+    }
+}
---
+++ BasicTodoList/ViewModels/ToDoItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicTodoList.ViewModels
+{
+    public partial class ToDoItemViewModel : ViewModelBase
+    {
+        private bool _isChecked;
+
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set { SetProperty(ref _isChecked, value); }
+        }
+
+        [ObservableProperty]
+        public partial string? Content { get; set; }
+    }
+}
